463009c7c5e39efbbbc1f248850a2c8693ceb2ab
Mehrdad <106564562+mehrdadmmz@users.noreply.github.com>
2024-10-30 00:32:53 -0700

    Create db.go

---
+++ db/db.go
@@ -0,0 +1,69 @@
+package db
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var DB *sql.DB
+
+// InitDB initializes the database and sets up a connection pool
+func InitDB() {
+	var err error
+	DB, err = sql.Open("sqlite3", "api.db")
+	if err != nil {
+		panic("could not connect to the database")
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+
+	createTables()
+}
+
+// createTables creates the necessary tables in the database if they do not exist
+func createTables() {
+	createUsersTable := `
+ CREATE TABLE IF NOT EXISTS users (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     email TEXT NOT NULL UNIQUE,
+     password TEXT NOT NULL
+ )
+`
+	_, err := DB.Exec(createUsersTable)
+	if err != nil {
+		panic("could not create users table")
+	}
+
+	createEventsTable := `
+ CREATE TABLE IF NOT EXISTS events (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     name TEXT NOT NULL,
+	 description TEXT NOT NULL,
+	 location TEXT NOT NULL,
+     dateTime DATETIME NOT NULL,
+	 user_id INTEGER,
+	 FOREIGN KEY (user_id) REFERENCES users(id)
+ )
+ `
+
+	_, err = DB.Exec(createEventsTable)
+	if err != nil {
+		panic("could not create events table")
+	}
+
+	createRegistrationTable := `
+ CREATE TABLE IF NOT EXISTS registrations (
+     id INTEGER PRIMARY KEY AUTOINCREMENT,
+     event_id INTEGER,
+     user_id INTEGER,
+     FOREIGN KEY (event_id) REFERENCES events(id),
+     FOREIGN KEY (user_id) REFERENCES users(id)
+ )
+ `
+
+	_, err = DB.Exec(createRegistrationTable)
+	if err != nil {
+		panic("could not create registrations table")
+	}
+}
